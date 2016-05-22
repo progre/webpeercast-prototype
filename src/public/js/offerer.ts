b1af04169e11c8a9fc589dfa4cd43af2ad59ddc3
progre <djyayutto@gmail.com>
2016-05-23 07:09:43 +0900

    WIP not working

--- src/public/js/offerer.ts
+++ src/public/js/offerer.ts
@@ -1,13 +1,65 @@
+import { EventEmitter } from "events";
 import PeerBase from "./peerbase";
 
 export default class Offerer extends PeerBase {
+    private signaling: EventEmitter;
+    private onSignalingIceCandidate: Function;
+
     constructor() {
         super();
 
         this.setDataChannel(this.pc.createDataChannel(""));
+        // send local candidate to remote
+        this.pc.onicecandidate = e => {
+            try {
+                let candidate = e.candidate;
+                if (!candidate) {
+                    return;
+                }
+                if (this.signaling != null) {
+                    this.signaling.emit("icecandidate", JSON.stringify(candidate));
+                }
+            } catch (e) {
+                console.error(e.stack || e);
+            }
+        };
+        // receive remote candidate to local
+        this.onSignalingIceCandidate = async (candidateJSON: string) => {
+            try {
+                let candidate = new RTCIceCandidate(JSON.parse(candidateJSON));
+                await this.addIceCandidate(candidate);
+            } catch (e) {
+                console.error(e.stack || e);
+            }
+        };
     }
 
-    async beginOffer() {
+    async open(signaling: EventEmitter) {
+        if (this.signaling != null) {
+            this.close();
+        }
+        this.signaling = signaling;
+        this.signaling.on("icecandidate", this.onSignalingIceCandidate);
+        let answer: RTCSessionDescription = await new Promise((resolve, reject) => {
+            let onAnswer = async (answerJSON: string) => {
+                try {
+                    this.removeListener("answer", onAnswer);
+                    resolve(new RTCSessionDescription(JSON.parse(answerJSON)));
+                } catch (e) {
+                    reject(e);
+                }
+            };
+            this.on("answer", onAnswer);
+
+            let offer = this.offer();
+            console.log("offer", offer);
+            this.signaling.emit("offer", JSON.stringify(offer));
+        });
+        console.log("Is it's answer for offer?", answer); // TODO: Is it's answer for offer?
+        await this.putAnswer(answer);
+    }
+
+    private async offer() {
         let offer: RTCSessionDescription = await (this.pc as any).createOffer();
         await this.pc.setLocalDescription(offer);
         // TODO: Implement for timeout
@@ -16,5 +68,11 @@ export default class Offerer extends PeerBase {
 
     putAnswer(answer: RTCSessionDescription) {
         return this.setRemoteDescription(answer);
+    }
+
+    close() {
+        super.close();
+
+        this.signaling.removeListener("icecandidate", this.onSignalingIceCandidate);
     }
 }
--- src/public/js/pc1.ts
+++ src/public/js/pc1.ts
@@ -6,17 +6,6 @@ let offerer = new EventEmitter();
 (window as any).remoteOfferer = offerer;
 
 let obj = new Offerer();
-obj.on("icecandidate", (e: RTCIceCandidateEvent) => {
-    try {
-        let candidate = e.candidate;
-        if (!candidate) {
-            return;
-        }
-        (window as any).remoteAnswerer.emit("icecandidate", JSON.stringify(candidate));
-    } catch (e) {
-        (window as any).failed(e);
-    }
-});
 obj.on("datachannelmessage", (evt: any) => {
     if (evt.data instanceof Blob) {
         fancy_log("*** pc2 sent Blob: " + evt.data + ", length=" + evt.data.size, "blue");
@@ -29,29 +18,5 @@ obj.on("datachannelmessage", (evt: any) => {
 (window as any).dc1 = obj.dc;
 
 export async function step0() {
-    try {
-        let offer = await obj.beginOffer();
-        (window as any).remoteAnswerer.emit("offer", JSON.stringify(offer));
-    } catch (e) {
-        (window as any).failed(e);
-    }
+    obj.open((window as any).remoteAnswerer);
 }
-
-// pc2.setLocal finished, call pc1.setRemote
-offerer.on("answer", async (answerJSON: string) => {
-    try {
-        let answer = new RTCSessionDescription(JSON.parse(answerJSON));
-        await obj.putAnswer(answer);
-    } catch (e) {
-        (window as any).failed(e);
-    }
-});
-
-offerer.on("icecandidate", async (candidateJSON: string) => {
-    try {
-        let candidate = new RTCIceCandidate(JSON.parse(candidateJSON));
-        await obj.addIceCandidate(candidate);
-    } catch (e) {
-        (window as any).failed(e);
-    }
-});
