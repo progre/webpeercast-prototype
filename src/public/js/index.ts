f80076c752b4d5fe7f8b23b7283b0078189b4013
progre <djyayutto@gmail.com>
2016-05-25 23:43:34 +0900

    WIP not working

--- src/public/js/index.ts
+++ src/public/js/index.ts
@@ -3,35 +3,24 @@ import "babel-polyfill";
 import "webrtc-adapter";
 import { EventEmitter } from "events";
 import Offerer from "./offerer.ts";
-import Answerer from "./answerer.ts";
 
 async function main() {
     let socket = new WebSocket(`ws://${location.hostname}`);
     socket.addEventListener("open", async () => {
         console.log("WebSocket open.");
-        let answererEE = new EventEmitter();
         let offererEE = new EventEmitter();
         socket.addEventListener("message", e => {
             let data = JSON.parse(e.data);
-            answererEE.emit(data.type, data.event);
             offererEE.emit(data.type, data.event);
         });
-        answererEE.on("icecandidate", (e: Event) =>
-            socket.send(JSON.stringify({ type: "icecandidate", event: e })));
         offererEE.on("icecandidate", (e: Event) =>
             socket.send(JSON.stringify({ type: "icecandidate", event: e })));
         offererEE.on("offer", (e: Event) =>
             socket.send(JSON.stringify({ type: "offer", event: e })));
-        answererEE.on("answer", (e: Event) =>
-            socket.send(JSON.stringify({ type: "answer", event: e })));
-        let answerer = new Answerer(answererEE);
-        answerer.on("datachannelopen", () => {
-            answerer.on("datachannelmessage", (e: RTCMessageEvent) => console.log("answerer", e));
-            answerer.dc.send("send to offerer");
-        });
         let offerer = await Offerer.create(offererEE);
+        offerer.on("datachannelmessage", (e: RTCMessageEvent) =>
+            console.log("offerer", e));
         offerer.on("datachannelopen", () => {
-            offerer.on("datachannelmessage", (e: RTCMessageEvent) => console.log("offerer", e));
             offerer.dc.send("send to answerer");
         });
     });
--- src/renderer/js/index.ts
+++ src/renderer/js/index.ts
@@ -1,8 +1,29 @@
 /// <reference path="../../../typings/index.d.ts" />
 import "babel-polyfill";
 import "webrtc-adapter";
+import { EventEmitter } from "events";
+import Answerer from "../../public/js/answerer.ts";
 
 async function main() {
+    let socket = new WebSocket(`ws://${location.hostname}`);
+    socket.addEventListener("open", async () => {
+        console.log("WebSocket open.");
+        let ee = new EventEmitter();
+        socket.addEventListener("message", e => {
+            let data = JSON.parse(e.data);
+            ee.emit(data.type, data.event);
+        });
+        ee.on("icecandidate", (e: Event) =>
+            socket.send(JSON.stringify({ type: "icecandidate", event: e })));
+        ee.on("answer", (e: Event) =>
+            socket.send(JSON.stringify({ type: "answer", event: e })));
+        let answerer = new Answerer(ee);
+        answerer.on("datachannelmessage", (e: RTCMessageEvent) =>
+            console.log("answerer", e));
+        answerer.on("datachannelopen", () => {
+            answerer.dc.send("send to offerer");
+        });
+    });
 }
 
 main().catch(e => console.error(e.stack != null ? e.stack : e));
